04e1d3e0b5a6ab92da94642cce431e1f06fc0204
estranossa <estranossa@gmail.com>
2023-07-04 17:49:36 +0300

    Add other files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name, char_class):
     if char_class == 'warrior':
@@ -82,7 +82,9 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    print(print.__doc__)
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
@@ -92,6 +94,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
